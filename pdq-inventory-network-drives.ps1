9835e08e1e6ed12a287bf8c158cfdaacc36a44d5
DRENREPUS <johnwhirsch@gmail.com>
2020-02-18 11:18:46 -0500

    Removed AD User lookup
    Instead the script now uses the user's local profile path folder name for simplicity.

--- pdq-inventory-network-drives.ps1
+++ pdq-inventory-network-drives.ps1
@@ -1,27 +1,20 @@
 # This share needs to be accessible by the PDQ Deploy user. This file must exist even if it is blank - the script will error out if it doesn't
-$replacements = import-csv -Path "Microsoft.PowerShell.Core\FileSystem::\\contoso.com\shares\IT$\PDQ-NetworkDrives-Replace.csv"
+$replacements = import-csv -Path "Microsoft.PowerShell.Core\FileSystem::\\int.contoso.com\shared\IT$\PDQRepository\PDQNetworkShares.csv" -ErrorAction Continue
 
 # true = Only scan user drives; false = Scan and update drives if they match any of the shares in the $replacements CSV
 $scanonly = $true;
 
 # false = Only save drives that don't exist in the replace column of the $replacements CSV
-$savealldrives = $false; 
-
-if((Get-Module ActiveDirectory)){ Write-Output "ActiveDirectory modules is already installed." }
-else{
-    if((Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ReleaseId).ReleaseId -ge 1809){ Add-WindowsCapability –online –Name "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0" | Out-Null }
-    else{ Enable-WindowsOptionalFeature -Online -FeatureName RSATClient-Roles-AD-Powershell | Out-Null }
-}
+$savealldrives = $true; 
 
 # Set PDQ Inventory to scan this path to collect all the inventory data as a scan after deployment in PDQ Deploy
 $RegPath = "HKLM:\SOFTWARE\Admin Arsenal\InventoryData"
 
-Get-ChildItem -Path $RegPath | ? { $_.Name -match "NetworkDrives" } | Remove-Item
+Get-ChildItem -Path $RegPath -ErrorAction SilentlyContinue | ? { $_.Name -match "NetworkDrives" } | Remove-Item
 
-function Get-UsernameFromSID {
-    param([string]$SID)
-    if( get-localuser -SID $($_.PSChildName) -ErrorAction SilentlyContinue ){ return "$((Get-LocalUser -SID $($_.PSChildName)).Name)-Local" }
-    else{ return (get-aduser -identity $($_.PSChildName)).SAMAccountName }
+function Get-UsernameFromHivePath {
+    param([string]$HivePath)
+    return $(select-string -InputObject $HivePath -Pattern '(?!=Users\\)([A-Za-z\s\-_])\w+(?=\\NTuser.dat)' | %{ $_.Matches[0].Groups[0].Value } )
 }
 
 function Save-PDQInventory {
@@ -39,8 +32,8 @@ function Save-PDQInventory {
 
 }
 
-try{ $UserProfiles = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*" | Where {$_.PSChildName -match "S-1-5-21-(\d+-?){4}$" } | Select-Object @{Name="SID"; Expression={$_.PSChildName}}, @{Name="UserHive";Expression={"$($_.ProfileImagePath)\NTuser.dat"}}, @{Name="UserName";Expression={  Get-UsernameFromSID -SID $_.PSChildName  }} }
-catch { Write-Output "Error getting list of user profiles"; Write-Output $error[0].Exception.Message; }
+try{ $UserProfiles = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*" | Where {$_.PSChildName -match "S-1-5-21-(\d+-?){4}$" } | Select-Object @{Name="SID"; Expression={$_.PSChildName}}, @{Name="UserHive";Expression={"$($_.ProfileImagePath)\NTuser.dat"}}, @{Name="UserName";Expression={  Get-UsernameFromHivePath -HivePath "$($_.ProfileImagePath)\NTuser.dat"  }} }
+catch { Write-Output "Error getting list of user profiles"; Write-Output $error[0].Exception.Message; exit 663; }
 
 Write-Output "Found the following $($UserProfiles.Count) user profiles:"; Write-Output $UserProfiles | FT; Write-Output "`n`n";
 
@@ -54,9 +47,9 @@ Foreach ($UserProfile in $UserProfiles) {
 
     # Manipulate the registry
     try{ $RegistryNetworkDrives = Get-Item -Path "Registry::HKEY_USERS\$($UserProfile.SID)\Network\*" }
-    catch { Write-Output "Error getting information from: $RegistryNetworkDrives"; Write-Output $error[0].Exception.Message; }
+    catch { Write-Output "Error getting information from: $RegistryNetworkDrives"; Write-Output $error[0].Exception.Message; exit 664; }
     
-    Write-Output "Getting all user network drives...`n`n"
+    Write-Output "Getting all user network drives for: $($UserProfile.UserName)"
 
     if($RegistryNetworkDrives.count -gt 0){
         foreach($RegistryNetworkDrive in $RegistryNetworkDrives){
@@ -88,7 +81,7 @@ Foreach ($UserProfile in $UserProfiles) {
                             }
                         }
                     }
-                }catch{  Write-Output "Error converting the network drive path"; Write-Output $error[0].Exception.Message; }
+                }catch{  Write-Output "Error converting the network drive path"; Write-Output $error[0].Exception.Message; exit 665; }
 
                 if($savealldrives -eq $false){
                     #Add the network drive to the PDQ inventory if it doesn't match one of the replacement drives; else save all network drives to PDQ inventory
@@ -96,7 +89,7 @@ Foreach ($UserProfile in $UserProfiles) {
                     if($foundReplacement -eq $false){ Save-PDQInventory -Username $($UserProfile.UserName) -NetworkDrive $($NetworkDrive) -NetworkDrivePath $($NetworkDrivePath) }
                 }else{ Save-PDQInventory -Username $($UserProfile.UserName) -NetworkDrive $($NetworkDrive) -NetworkDrivePath $($NetworkDrivePath) }
 
-            }catch{ Write-Output "Error getting information from: $RegistryNetworkDrives"; Write-Output $error[0].Exception.Message; }
+            }catch{ Write-Output "Error getting information from: $RegistryNetworkDrives"; Write-Output $error[0].Exception.Message; exit 667; }
         }
     }
 
